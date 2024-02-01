1cd41aaf1fd4db5069ec35793e952b6da9f0cbf2
mataberat <aris@onboardcrewapp.com>
2024-02-01 14:36:46 +0700

    Fix remove current .env file directly on script

--- scripts/aws_ssm_generate_env_file.sh
+++ scripts/aws_ssm_generate_env_file.sh
@@ -1,19 +1,21 @@
-  #!/bin/bash
-  
-  local prefix="$1"
+#!/bin/bash
 
-  local params=$(aws ssm get-parameters-by-path \
-  --path "${prefix}" \
-  --recursive --with-decryption \
-  --query "Parameters[*].{Name:Name,Value:Value}" \
-  --output json)
+rm -rf .env*
 
-  echo -n "" > .env
+local prefix="$1"
 
-  for row in $(echo "${params}" | jq -r '.[] | @base64'); do
-      _jq() {
-      echo ${row} | base64 --decode | jq -r ${1}
-      }
-    key_name=$(echo $(_jq '.Name') | sed "s|^${prefix}/||")
-    echo ${key_name}=$(echo $(_jq '.Value')) >> .env
-  done
+local params=$(aws ssm get-parameters-by-path \
+--path "${prefix}" \
+--recursive --with-decryption \
+--query "Parameters[*].{Name:Name,Value:Value}" \
+--output json)
+
+echo -n "" > .env
+
+for row in $(echo "${params}" | jq -r '.[] | @base64'); do
+    _jq() {
+    echo ${row} | base64 --decode | jq -r ${1}
+    }
+  key_name=$(echo $(_jq '.Name') | sed "s|^${prefix}/||")
+  echo ${key_name}=$(echo $(_jq '.Value')) >> .env
+done
