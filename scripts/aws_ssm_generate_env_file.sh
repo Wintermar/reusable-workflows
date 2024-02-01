d0a52a9d67865171a9a140007c82cc10bb69c3be
mataberat <aris@onboardcrewapp.com>
2024-02-01 14:35:18 +0700

    Fix script to generate .env file

--- scripts/aws_ssm_generate_env_file.sh
+++ scripts/aws_ssm_generate_env_file.sh
@@ -1,24 +1,19 @@
-#!/bin/bash
+  #!/bin/bash
+  
+  local prefix="$1"
 
-prefix="$1"
-target="$2"
+  local params=$(aws ssm get-parameters-by-path \
+  --path "${prefix}" \
+  --recursive --with-decryption \
+  --query "Parameters[*].{Name:Name,Value:Value}" \
+  --output json)
 
-params=$(aws ssm get-parameters-by-path \
---path "${prefix}" \
---recursive --with-decryption \
---query "Parameters[*].{Name:Name,Value:Value}" \
---output json)
+  echo -n "" > .env
 
-echo -n "" > $(pwd)/$target
-
-for row in $(echo "${params}" | jq -r '.[] | @base64'); do
-    _jq() {
-    echo ${row} | base64 --decode | jq -r ${1}
-    }
-  key_name=$(echo $(_jq '.Name') | sed "s|^${prefix}/||")
-  echo ${key_name}=$(echo $(_jq '.Value')) >> $(pwd)/$target
-done
-
-if [ $(cat $(pwd)/$target | wc -l) -eq 0 ]; then
-  echo "AWS SSM generated file is empty. Exiting with error." && exit 1
-fi
+  for row in $(echo "${params}" | jq -r '.[] | @base64'); do
+      _jq() {
+      echo ${row} | base64 --decode | jq -r ${1}
+      }
+    key_name=$(echo $(_jq '.Name') | sed "s|^${prefix}/||")
+    echo ${key_name}=$(echo $(_jq '.Value')) >> .env
+  done
